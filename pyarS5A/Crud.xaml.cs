71b05e618830b3bcc008d4089748df0ed59cf446
PabloYar <pabloyardeveloper@gmail.com>
2023-01-17 19:53:19 -0500

    Agregar archivos de proyecto.

---
+++ pyarS5A/Crud.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace pyarS5A
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Crud : ContentPage
+    {
+        public const string Url = "http://192.168.167.190:8080/projects/uisrael2023/post.php";
+
+        public Crud()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient client = new WebClient();
+                var parametros = new System.Collections.Specialized.NameValueCollection();
+                parametros.Add("codigo", txtCodigo.Text);
+                parametros.Add("nombre", txtNombre.Text);
+                parametros.Add("apellido", txtApellido.Text);
+                parametros.Add("edad", txtEdad.Text);
+
+                client.UploadValues(Url, "POST", parametros);
+                    
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Error", ex.Message, "Cerrar");
+}
+        }
+
+        private async void btnRegresar_Clicked(object sender, EventArgs e)
+        {
+            //Navigation.PopAsync(new MainPage());
+
+        }
+    }
+}
---
+++ pyarS5A/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using pyarS5A.ws;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace pyarS5A
+{
+    public partial class MainPage : ContentPage
+    {
+        public const string Url = "http://192.168.167.190:8080/projects/uisrael2023/post.php";
+        private readonly HttpClient _client = new HttpClient();
+        private ObservableCollection<Datos> _post;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Get_Clicked(object sender, EventArgs e)
+        {
+            var content = await _client.GetStringAsync(Url);
+            List<Datos> posts = JsonConvert.DeserializeObject<List<Datos>>(content);
+            _post = new ObservableCollection<Datos>(posts);
+            MyListView.ItemsSource = posts;
+        }
+    }
+}
